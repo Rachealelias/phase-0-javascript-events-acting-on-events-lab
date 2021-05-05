79a7b6a07d3c483f9ad4311b113036ccb2166ed5
Rachealelias <rachealsamuel708@gmail.com>
2021-05-05 14:37:34 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === 40) {
+      moveDodgerLeft();
+    }
+  }); 
+
+  function moveDodgerRight(){
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+
+      if (left < 400){
+          dodger.style.left = `${left + 1}px`;
+      }
+  }
+
+  document.addEventListener("keydown", function(e){
+     if (e.key === 40){
+         moveDodgerRight();
+     }
+  });
